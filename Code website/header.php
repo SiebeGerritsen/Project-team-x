4f99f02d72ac4518ef72aa7b08b03f070020e461
moesware <moesware@hotmail.com>
2018-10-10 13:36:58 +0200

    hjyudfcjh,v

--- Code website/header.php
+++ Code website/header.php
@@ -1,15 +1,99 @@
-<?php
-
-?>
-
-    <!DOCTYPE html>
+<!DOCTYPE html>
     <html>
     <head>
-        <title>webshop</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>gezinshuis</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bootstrap.min.css">
+        <link rel="stylesheet" href="custom.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+        <!-- bootstrap source-->
+
+
+
+
+
     </head>
 <body>
 
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Contact <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Overeenkomst</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">Beflap</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registreren</a></li>
+                <li><a href="#" data-toggle="modal" data-target="#login-modal"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <h3>Collapsible Navbar</h3>
+    <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).
+    <p>Only when the button is clicked, the navigation bar will be displayed.</p>
+</div>
+<!--
+<a href="#" data-toggle="modal" data-target="#login-modal">Login</a> -->
+
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog">
+        <div class="loginmodal-container">
+            <h1>Login to Your Account</h1><br>
+            <form>
+                <input type="text" name="user" placeholder="Username">
+                <input type="password" name="pass" placeholder="Password">
+                <input type="submit" name="login" class="login loginmodal-submit" value="Login">
+            </form>
+
+            <div class="login-help">
+                <a href="#">Register</a> - <a href="#">Forgot Password</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog">
+        <div class="loginmodal-container">
+            <h1>Login to Your Account</h1><br>
+            <form>
+                <input type="text" name="user" placeholder="Username">
+                <input type="password" name="pass" placeholder="Password">
+                <input type="submit" name="login" class="login loginmodal-submit" value="Login">
+            </form>
+
+            <div class="login-help">
+                <a href="#">Register</a> - <a href="#">Forgot Password</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!--
 <header>
     <nav>
         <div class="main-wrapper">
@@ -22,7 +106,7 @@
 
             <div class="nav-login">
                 <?php /*zodra ik ben ingelogd wil ik alleen de logout button zien*/
-                if (isset($_SESSION['u_id'])) {
+               /* if (isset($_SESSION['u_id'])) {
 
                     echo '<form action="includes/logout.inc.php" method="POST">
                         <button type="submit" name="submit">Logout</button>
