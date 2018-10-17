8bd2f97d18129acc6320b0dbdb9a86c2a5c02bc7
SiebeGerritsen <siebe.1@hotmail.com>
2018-10-17 14:15:04 +0200

    2e header

--- Code website/config.php
+++ Code website/config.php
@@ -2,7 +2,7 @@
 
 return[
     'database'=> [
-        'name' => 'events',
+        'name' => 'gasthuis',
         'username' => 'root',
         'password' => 'hallo531',
         'connection' => 'mysql:host=127.0.0.1',
